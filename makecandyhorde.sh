7576007cf530bc12aa7b9076d4a28b5e25483a94
Markus Iser <markus.iser@kit.edu>
2018-11-20 14:36:26 +0100

    update build

--- makecandyhorde.sh
+++ makecandyhorde.sh
@@ -8,7 +8,7 @@ git submodule init
 git submodule update
 mkdir -p build
 cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug ..
 make candy
 cd ../..
 
